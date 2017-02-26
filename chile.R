6b89af92d7a74e38b6b49b92f1917056ea8d6cde
jpl33 <pladott1@gmail.com>
2017-02-26 22:41:10 +0200

    factorized _income_ AND _population_

--- chile.R
+++ chile.R
@@ -11,15 +11,24 @@ require(car)
 require(dummies)
 
 dff<-read.csv("clean_chile.csv")
+dff[,"income_fact"]<-cut(dff$income, breaks = 8)
+dff[,"population_fact"]<-cut(dff$population, breaks = 10)
+
 
 ##############################################################################
 library(dummies)
 
 #create a dummy data frame
-dff_pca<- dummy.data.frame(dff[,-8], names = c("region","population","sex","education"))
+dff_pca<- dummy.data.frame(dff[,-c(8,9,10)], names = c("region","sex","education"))
+dff_pca2<- dummy.data.frame(dff[,-c(2,6,8)], names = c("region","population_fact","sex","education","income_fact"))
 
 
 prin_comp <- prcomp(dff_pca, scale. = T)
+prin_comp2<- prcomp(dff_pca2, scale. = T)
 screeplot(prin_comp,type="lines")
+screeplot(prin_comp2,type="lines")
+
+summary(prin_comp)
+summary(prin_comp2)
 
 
