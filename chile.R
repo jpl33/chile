627479383a1f067b2d51f8032be2b516b3b98876
jpl33 <pladott1@gmail.com>
2017-02-26 21:21:01 +0200

    facotrized _population_ variable. it seems to improve PCA

--- chile.R
+++ chile.R
@@ -16,7 +16,10 @@ dff<-read.csv("clean_chile.csv")
 library(dummies)
 
 #create a dummy data frame
-dff_pca<- dummy.data.frame(dff[,-8], names = c("region","sex","education"))
+dff_pca<- dummy.data.frame(dff[,-8], names = c("region","population","sex","education"))
+
 
 prin_comp <- prcomp(dff_pca, scale. = T)
 screeplot(prin_comp,type="lines")
+
+
